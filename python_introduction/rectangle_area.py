e3e9e6d215921acc98196197f5a91bd5dd900ab5
adanhafsa <hafsaadan25678@gmail.com>
2025-06-13 12:58:35 +0300

    fixed the typo: uppercase in Area

--- python_introduction/rectangle_area.py
+++ python_introduction/rectangle_area.py
@@ -3,6 +3,6 @@ length = 10
 width = 5 
 
 # calculate the area of the rectangle using formula Area = length * width
-Area = length * width 
+area = length * width 
 # print the result in the specified format
-print(f"The area of the rectange is: {Area}")
+print(f"The area of the rectange is: {area}")
