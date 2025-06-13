4db5ff8fed6fff6cd006ff507da31f7d6596cd63
adanhafsa <hafsaadan25678@gmail.com>
2025-06-13 11:13:52 +0300

    Add and commit changes

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,7 +1,8 @@
-principal = 1000 # Principal amount
-rate = 5 # Rate of interest in percent
-time = 3 # Time in years
-# Calculate simple interest
-simple_interest = (principal * rate * time) / 100
-# Print the result
-print(f"The simple interest is: {simple_interest}")
+# simple_interest.py
+principal = 1000 
+rate = 5
+time = 3
+# calculate simple interest
+interest = (principal * rate * time) / 100
+# print the rusult
+print(f"simple interest: {interest}")
